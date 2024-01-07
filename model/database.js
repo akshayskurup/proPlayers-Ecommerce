a113878243c11d50793f74766218ee3fba4716b3
Akshay <akshayskurup@gmail.com>
2024-01-07 21:47:53 +0530

    changed db

--- model/database.js
+++ model/database.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
 
 
+// let connnectDB = async ()=>{
+//     try{
+//         await mongoose.connect("mongodb://0.0.0.0:27017/ProPlayers_Ecommerce",{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//         console.log("DataBase connected successfully....")
+//     }
+//     catch(err){
+//         console.log("Error connecting to dataBase",err.message )
+//         process.exit(1)
+//     }
+// }
+
 let connnectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb://0.0.0.0:27017/ProPlayers_Ecommerce",{
+        await mongoose.connect("mongodb://akshayskurup:uuqvWOc0lnzQrbw6@ac-bxoq78f-shard-00-00.ph4xbey.mongodb.net:27017,ac-bxoq78f-shard-00-01.ph4xbey.mongodb.net:27017,ac-bxoq78f-shard-00-02.ph4xbey.mongodb.net:27017/?ssl=true&replicaSet=atlas-j61sz7-shard-0&authSource=admin&retryWrites=true&w=majority",{
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
@@ -14,6 +28,7 @@ let connnectDB = async ()=>{
         process.exit(1)
     }
 }
+
 
 
 module.exports = connnectDB
